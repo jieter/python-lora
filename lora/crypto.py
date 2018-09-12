31ea90c875ea25036dd6d950a28ab011e1ab72d6
Jan Pieter Waagmeester <jieter@zostera.nl>
2018-09-12 20:29:28 +0200

    Blacken code

--- lora/crypto.py
+++ lora/crypto.py
@@ -11,15 +11,15 @@ DOWN_LINK = 1
 
 
 def to_bytes(s):
-    '''PY2/PY3 compatible way to convert to something cryptography understands'''
-    if sys.version_info < (3, ):
-        return ''.join(map(chr, s))
+    """PY2/PY3 compatible way to convert to something cryptography understands"""
+    if sys.version_info < (3,):
+        return "".join(map(chr, s))
     else:
         return bytes(s)
 
 
 def loramac_decrypt(payload_hex, sequence_counter, key, dev_addr, direction=UP_LINK):
-    '''
+    """
     LoraMac decrypt
 
     Which is actually encrypting a predefined 16-byte block (ref LoraWAN
@@ -35,7 +35,7 @@ def loramac_decrypt(payload_hex, sequence_counter, key, dev_addr, direction=UP_L
 
     This method is based on `void LoRaMacPayloadEncrypt()` in
     https://github.com/Lora-net/LoRaMac-node/blob/master/src/mac/LoRaMacCrypto.c#L108
-    '''
+    """
     key = unhexlify(key)
     dev_addr = unhexlify(dev_addr)
     buffer = bytearray(unhexlify(payload_hex))
@@ -51,37 +51,37 @@ def loramac_decrypt(payload_hex, sequence_counter, key, dev_addr, direction=UP_L
     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
 
     def aes_encrypt_block(aBlock):
-        '''
+        """
         AES encrypt a block.
         aes.encrypt expects a string, so we convert the input to string and
         the return value to bytes again.
-        '''
+        """
         encryptor = cipher.encryptor()
 
-        return bytearray(
-            encryptor.update(to_bytes(aBlock)) + encryptor.finalize()
-        )
+        return bytearray(encryptor.update(to_bytes(aBlock)) + encryptor.finalize())
 
     # For the exact definition of this block refer to
     # 'chapter 4.3.3.1 Encryption in LoRaWAN' in the LoRaWAN specification
-    aBlock = bytearray([
-        0x01,                             # 0 always 0x01
-        0x00,                             # 1 always 0x00
-        0x00,                             # 2 always 0x00
-        0x00,                             # 3 always 0x00
-        0x00,                             # 4 always 0x00
-        direction,                        # 5 dir, 0 for uplink, 1 for downlink
-        dev_addr[3],                      # 6 devaddr, lsb
-        dev_addr[2],                      # 7 devaddr
-        dev_addr[1],                      # 8 devaddr
-        dev_addr[0],                      # 9 devaddr, msb
-        sequence_counter & 0xff,          # 10 sequence counter (FCntUp) lsb
-        (sequence_counter >> 8) & 0xff,   # 11 sequence counter
-        (sequence_counter >> 16) & 0xff,  # 12 sequence counter
-        (sequence_counter >> 24) & 0xff,  # 13 sequence counter (FCntUp) msb
-        0x00,                             # 14 always 0x01
-        0x00                              # 15 block counter
-    ])
+    aBlock = bytearray(
+        [
+            0x01,  # 0 always 0x01
+            0x00,  # 1 always 0x00
+            0x00,  # 2 always 0x00
+            0x00,  # 3 always 0x00
+            0x00,  # 4 always 0x00
+            direction,  # 5 dir, 0 for uplink, 1 for downlink
+            dev_addr[3],  # 6 devaddr, lsb
+            dev_addr[2],  # 7 devaddr
+            dev_addr[1],  # 8 devaddr
+            dev_addr[0],  # 9 devaddr, msb
+            sequence_counter & 0xff,  # 10 sequence counter (FCntUp) lsb
+            (sequence_counter >> 8) & 0xff,  # 11 sequence counter
+            (sequence_counter >> 16) & 0xff,  # 12 sequence counter
+            (sequence_counter >> 24) & 0xff,  # 13 sequence counter (FCntUp) msb
+            0x00,  # 14 always 0x01
+            0x00,  # 15 block counter
+        ]
+    )
 
     # complete blocks
     while size >= 16:
@@ -105,5 +105,5 @@ def loramac_decrypt(payload_hex, sequence_counter, key, dev_addr, direction=UP_L
 
 
 def generate_appskey():
-    '''Generate a random secret key'''
-    return ''.join('{:02X}'.format(x) for x in random.sample(range(255), 16))
+    """Generate a random secret key"""
+    return "".join("{:02X}".format(x) for x in random.sample(range(255), 16))
--- tests/import_fixtures.py
+++ tests/import_fixtures.py
@@ -5,16 +5,16 @@ from lora.payload import LoRaPayload
 
 # Import fixtures from a file containing
 #
-dev_addr = sys.argv[1] or '14000122'
-fixture_filename = '{}.txt'.format(dev_addr)
+dev_addr = sys.argv[1] or "14000122"
+fixture_filename = "{}.txt".format(dev_addr)
 
 if not os.exists(fixture_filename):
-    print('File containing fixtures does not exist ({})'.format(fixture_filename))
+    print("File containing fixtures does not exist ({})".format(fixture_filename))
     sys.exit()
 
 fixtures = open(fixture_filename)
 
-filename_fmt = 'fixtures/{}/payload_{}-{}.xml'
+filename_fmt = "fixtures/{}/payload_{}-{}.xml"
 LORA_XML_HEADER = '<?xml version="1.0" encoding="UTF-8"?><DevEUI_uplink xmlns="http://uri.actility.com/lora">'
 enc = 1
 plain = 1
@@ -28,14 +28,14 @@ for item in fixtures.readlines():
 
     payload = LoRaPayload(item)
 
-    if payload.FPort == '1':
+    if payload.FPort == "1":
         # not encrypted
-        filename = filename_fmt.format(dev_addr, 'plaintext', plain)
+        filename = filename_fmt.format(dev_addr, "plaintext", plain)
         plain += 1
     else:
-        filename = filename_fmt.format(dev_addr, 'encrypted', enc)
+        filename = filename_fmt.format(dev_addr, "encrypted", enc)
         enc += 1
         # empty file for expected plaintexts.
-        open(filename.replace('.xml', '.txt'), 'w').write('')
+        open(filename.replace(".xml", ".txt"), "w").write("")
 
-    open(filename, 'w').write(item.replace('><', '>\n<'))
+    open(filename, "w").write(item.replace("><", ">\n<"))
